ba08a4212b0c7f021a86ce9b773b60f867e0776a
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-10 11:32:15 +0600

    navber added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
+import Navber from '../pages/Shared/Navber/Navber';
 
 const Main = () => {
     return (
         <div>
-            
+            <Navber></Navber>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider  } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import router from './Routers/Routers'
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}></RouterProvider>
+  <div className='max-w-7xl	mx-auto'>
+    <RouterProvider router={router}></RouterProvider>
+  </div>
+
 )
