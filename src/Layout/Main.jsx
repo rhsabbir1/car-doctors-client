013b5c56e1e5cd77f6d64513302d995cafdbcec8
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-10 11:20:00 +0600

    footer section added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
-            Bismillah
+            
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import logo from '../../../assets/logo.svg';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div>
+                <img  width="80" height="80" viewBox="0 0 24 24" src={logo} alt="" />
+                
+                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+            </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
