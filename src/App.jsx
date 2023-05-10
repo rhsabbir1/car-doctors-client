fff1e5cb16baa3b1bc40820292bf287cadc98e31
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-10 11:03:36 +0600

    car doctors cilent side start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hlwwwwww</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider  } from 'react-router-dom'
+import router from './Routers/Routers'
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}></RouterProvider>
+)
